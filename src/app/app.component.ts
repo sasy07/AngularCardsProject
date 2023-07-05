bec03862322c23c2f5df3fb68225d93111caf8d3
mrlost <mrlost@yahoo.com>
2023-07-05 12:48:13 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularCardsProject';
+  posts = [
+    {
+      title: 'tree',
+      imageUrl: 'assets/images/tree.jpeg',
+      userName: 'nature',
+      content: 'I saw neat tree',
+    },
+    {
+      title: 'mountain',
+      imageUrl: 'assets/images/mountain.jpeg',
+      userName: 'mountain',
+      content: 'I saw neat mountain',
+    },
+    {
+      title: 'bicycle',
+      imageUrl: 'assets/images/biking.jpeg',
+      userName: 'biking',
+      content: 'I saw biking',
+    },
+  ];
+
+  getPosts(){
+    return this.posts;
+  }
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit {
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() content = '';
+  @Input() userName = '';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
